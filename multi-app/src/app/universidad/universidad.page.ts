779c0f6a103ddf8748537676ec250ba0210a9568
wabreufmp <wabreu@fmp.com.do>
2023-10-25 17:13:22 -0400

    Completando vista de universidades

--- multi-app/src/app/universidad/universidad.page.ts
+++ multi-app/src/app/universidad/universidad.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-universidad',
@@ -6,9 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./universidad.page.scss'],
 })
 export class UniversidadPage implements OnInit {
+  country: string = '';
+  universities: any[] = [];
 
-  constructor() { }
 
+  constructor(private http: HttpClient) { }
+  uniInfo(){
+    console.log('Boton presionado');
+
+    const apiUrl = `http://universities.hipolabs.com/search?country=${this.country}`;
+
+    this.http.get(apiUrl).subscribe((response: any) =>{
+      console.log('Respuesta de la API: ', response);
+      this.universities = response;
+    });
+  }
   ngOnInit() {
   }
 
