e3644bbd23f442a8eacd2708605c4701baf41bd3
Thammy Franceily Vargas Acosta <20211809@itla.edu.do>
2023-10-25 23:33:18 -0400

    Cambiando errores en vista universidad y agregando vista Clima

--- multi-app/src/app/clima/clima.page.ts
+++ multi-app/src/app/clima/clima.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
 
 @Component({
   selector: 'app-clima',
@@ -7,7 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ClimaPage implements OnInit {
 
-  constructor() { }
+  degrees: any[] = [];
+  constructor(private http: HttpClient) { }
+
+  weather(){
+    console.log('Boton presionado');
+
+    const apiUrl = `https://api.weatherbit.io/v2.0/current?lat=18.5001&lon=-69.9885&key=d9a7e5f8d38547aea32b694543ffcef0&include=minutely`;
+    
+    this.http.get(apiUrl).subscribe((response: any) => {
+      console.log('Respuesta de la API: ', response);
+      this.degrees = response;
+    })
+
+  }
 
   ngOnInit() {
   }
